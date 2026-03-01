4d0e04abaae376432af163ac08baf6ca8a819b42
Nicol Vanessa Corral <nicolvanessa2126@gmail.com>
2026-03-01 00:09:16 -0500

    fix: set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/smile-design-studio/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
